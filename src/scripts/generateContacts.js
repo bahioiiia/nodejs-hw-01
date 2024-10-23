d211331ba5124aa889eb1185a737a760fee950f4
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-10-23 05:51:39 +0300

    *

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+const generateContacts = async (number) => {
+    const contacts = await readContacts();
+    const newContacts = Array(number).fill(0).map(createFakeContact);//.map(()=>createFakeContact());
+    const result = [...contacts, ...newContacts];
+    await writeContacts(result);
+    
+    console.log(result);
+ };
 
 generateContacts(5);
